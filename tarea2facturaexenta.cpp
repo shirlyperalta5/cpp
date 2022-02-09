b4341532b8c9992973896b6bd3787666a47fbeb0
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-02-08 19:53:49 -0600

    Add files via upload

---
+++ tarea2facturaexenta.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	float total=0, subtotal=0,impuesto=0;
+	
+	char sinpago;
+	
+	cout <<"Ingrese el valor de la compra:"<<endl;
+	cin>>subtotal;
+	
+	cout << " la factura esta excenta de impuesto? s/n:"<<endl;
+	cin >>sinpago;
+	if(sinpago=='n'||sinpago=='N')
+	{	
+	impuesto=subtotal*0.15;
+	total=subtotal+impuesto;
+	}
+	else
+	if(sinpago=='s'||sinpago=='S'){
+	total=subtotal;
+	}
+cout << " el total a pagar es de:"<<total <<endl;
+			
+	system ("pause");
+	return 0;
+}
+
