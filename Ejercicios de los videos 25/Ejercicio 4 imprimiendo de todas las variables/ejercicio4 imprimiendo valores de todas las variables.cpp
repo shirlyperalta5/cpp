7e6b72fe5c4aa71f2dcf3e0656804a3f5d3ea668
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-02-16 21:13:23 -0600

    Add files via upload

---
+++ Ejercicios de los videos 25/Ejercicio 4 imprimiendo de todas las variables/ejercicio4 imprimiendo valores de todas las variables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+// Shirly Peralta 20112000781
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char const *argv[])
+ {
+
+	bool valorBoolean;
+	valorBoolean = false;
+	
+	int valorEntero = 15;
+	double valorDouble = 20.99;
+	string valorString = "Hola como estan?";
+	char valorChar = 'B';
+	
+	cout << "Valor Boolean:" << valorBoolean << endl;
+	cout << "Valor Entero:" << valorEntero << endl;
+	cout << "Valor Double:" << valorDouble << endl;
+	cout << "valor String: " <<valorString <<endl;
+	cout << "Valor Char:" << valorChar << endl;
+			
+	return 0;
+
+}
