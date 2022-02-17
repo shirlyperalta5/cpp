e73599e8bbbb8afbdc26751d333adeee006b5638
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-02-16 21:06:45 -0600

    Add files via upload

---
+++ Ejercicios de los videos 25/Ejercicio 3 variables/ejercicio3variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// shirly peralta 20112000781
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char const *argv[]) 
+{
+	int numeroEntero;
+	numeroEntero = 15;
+	
+	cout << "Entero: " numeroEntero;
+	
+	numeroEntero = 18;
+	
+	cout << "Entero modificado: " numeroEntero;
+
+	cout << endl;
+
+	return 0;
+}
