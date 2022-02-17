ee692d8d43b4dbcece700f1b45ebef54671f3c60
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-02-16 20:59:57 -0600

    Add files via upload

---
+++ Ejercicios de los videos 25/Ejercicio 2 su nombre/ejercicio2mi nombre.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//Shirly Peralta 20112000781
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	string nombre;
+	
+	cout << "Ingrese el nombre: ";
+	cin >> nombre;
+	
+	cout << endl;
+	cout << "Su nombre es: " << nombre;
+	cout << endl;
+
+	return 0;
+}
