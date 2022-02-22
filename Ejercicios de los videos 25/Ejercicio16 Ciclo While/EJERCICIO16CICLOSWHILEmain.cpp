9c053945b6ae7dc0fee15841afc5c7249c29d6a0
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-02-21 19:05:33 -0600

    Add files via upload

---
+++ Ejercicios de los videos 25/Ejercicio16 Ciclo While/EJERCICIO16CICLOSWHILEmain.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream> 
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int a = 5;
+	int b = 2;
+
+	while( a > b) {
+		cout << " Ingrese el valor a: "<< endl;
+		cin >> a;
+
+}
+cout << endl;
+cout << "Saliste del ciclo";
+
+
+		return 0;
+}
+	
+
+
