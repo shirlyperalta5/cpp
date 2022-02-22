170481e25eec92f786a437df576ce2b006776aa6
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-02-21 20:06:08 -0600

    Add files via upload

---
+++ Ejercicios de los videos 25/Ejercicio22 Ciclos For/EJERCICIO22CICLOFORmain.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	for (int indice = 745; indice <= 1000; indice ++)
+	{
+		cout << "valor de la variable indice " << indice << endl;
+	}
+
+	int a = 0;
+	
+	a + a + 1;
+	
+	a++;
+		
+return 0;
+
+}
+	
+
