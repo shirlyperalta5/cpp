dc77ea9ac3868fb4435c50f4529cec85301e7ee2
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-03-16 20:36:57 -0600

    Add files via upload

---
+++ Tarea 7/main.cpp
@@ -0,0 +1,16 @@
+/*
+Alumna: Shirly Sarai Peralta Reyes
+Numero de cuenta: 20112000781
+*/
+
+#include <iostream>
+#include "Menu.h"
+
+using namespace std;
+
+int main ()
+{	
+	menu();
+	
+	return 0;
+}
